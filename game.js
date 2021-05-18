0239d4226498aa40e07b79cd04155e17f2177fbf
akif shaikh <66055292+ZACK8149@users.noreply.github.com>
2021-05-18 12:20:20 +0530

    for mobile only

--- game.js
+++ game.js
@@ -7,15 +7,6 @@ var userClickedPattern = [];
 var started = false;
 var level = 0;
 
-$(document).keypress(function() {
-  if (!started) {
-    $("#level-title").text("Level " + level);
-    nextSequence();
-    started = true;
-
-
-  }
-});
 $(document).click(function() {
     if (!started) {
       $("#level-title").text("Level " + level);
